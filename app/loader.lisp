0ec0f3d17ac2d0d542b8d96a0bb9b7ab86c2c123
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-04-25 18:44:43 -0700

    not actually including :sockets in loader since all comm goes through cl-async.

--- app/loader.lisp
+++ app/loader.lisp
@@ -1,16 +1,16 @@
 ;; -----------------------------------------------------------------------------
 ;; main setup
 ;; -----------------------------------------------------------------------------
-(require :sockets)
 ;; setup ASDF
 (require :asdf)
 (let ((cur-dir (truename *default-pathname-defaults*)))
   (setf asdf:*central-registry* (list cur-dir (truename "./app/asdf/registry/") (truename "./app/")))
   (setf asdf::*user-cache* (list (merge-pathnames #p"app/asdf/" cur-dir) "cache" :IMPLEMENTATION)))
-;; setup compiler
-;(ext:install-c-compiler)
-;(setf c:*cc* "gcc.exe")
-;(setf c:*user-cc-flags* "")
+;; setup sockets. we really don't need the full socket lib though since all comm
+;; goes through cl-async, but we do need to fool ECL into thinking the
+;; sb-bsd-sockets package exists.
+;(require :sockets)
+(defpackage sb-bsd-sockets)
 
 ;; load main libraries/initialize
 (asdf:operate 'asdf:load-op :turtl-core)
