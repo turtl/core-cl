87f2330e71c5af7ffc3d31c0229331a0fc1db8fd
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-05-03 01:18:09 -0700

    making test order a bit more natural

--- test/msg.js
+++ test/msg.js
@@ -1,26 +1,5 @@
 var TurtlEvent	=	require('../TurtlEvent');
 
-var test_response	=	function()
-{
-	var Event	=	new TurtlEvent.Event();
-	var Remote	=	new TurtlEvent.Remote();
-
-	// pipe remote events into local trigger
-	Remote.bind(Event.trigger);
-
-	var send_ping	=	function()
-	{
-		Remote.send({ev: 'ping'}, function(event) {
-			console.log('js: got response: ', event);
-			setTimeout(send_ping, 4000);
-		});
-	};
-
-	TurtlEvent.start();
-
-	setTimeout(send_ping, 100);
-};
-
 var test_bind	=	function()
 {
 	var Event	=	new TurtlEvent.Event();
@@ -37,6 +16,27 @@ var test_bind	=	function()
 	Event.bind('pong', function(event) {
 		setTimeout(send_ping, 4000);
 	});
+
+	TurtlEvent.start();
+
+	setTimeout(send_ping, 100);
+};
+
+var test_response	=	function()
+{
+	var Event	=	new TurtlEvent.Event();
+	var Remote	=	new TurtlEvent.Remote();
+
+	// pipe remote events into local trigger
+	Remote.bind(Event.trigger);
+
+	var send_ping	=	function()
+	{
+		Remote.send({ev: 'ping'}, function(event) {
+			console.log('js: got response: ', event);
+			setTimeout(send_ping, 4000);
+		});
+	};
 
 	TurtlEvent.start();
 
