13e2200eb0435158467d47de894a66342f5fbf2d
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-06-04 01:11:28 -0700

    working decrpt! verified working against version 0,3,4,5. still need to test 1,2

--- app/library/crypto.lisp
+++ app/library/crypto.lisp
@@ -5,11 +5,17 @@
 (define-condition crypto-error (turtl-error) ()
   (:documentation "General crypto error."))
 
-(define-constant +crypto-version+ 5
+(define-condition crypto-authentication-failure (crypto-error) ()
+  (:documentation "Authentication of HMAC failed."))
+
+(define-constant +crypto-version+ 6
   :documentation "Our (de)serialization version."
   :test '=)
 
+
 ;; -----------------------------------------------------------------------------
+;; Payload description options (stored in plaintext with each encrypted item).
+;;
 ;; NOTE: never inject items into these lists...only append them!!!!
 ;; NOTE: these lists can only support 256 items each!!!!
 ;; -----------------------------------------------------------------------------
@@ -25,13 +31,14 @@
 (define-constant +kdf-index+
   #(#(:sha256 2 64))
   :test 'equalp)
-;; -----------------------------------------------------------------------------
 
 ;; these are defaults, and the values are indxes in the above arrays
 (define-constant +default-cipher+ 0)
 (define-constant +default-block+ 1)
 (define-constant +default-padding+ 1)
 (define-constant +default-kdf-mode+ 0)
+;; -----------------------------------------------------------------------------
+
 
 (defun decode-payload-description (version desc)
   "Given a serialization version and a payload description, pull out any
@@ -40,7 +47,7 @@
     (when (<= 1 version)
       (setf (getf res :cipher) (aref +cipher-index+ (aref desc 0)))
       (setf (getf res :block-mode) (aref +block-index+ (aref desc 1)))
-      (when (<= 4 version)
+      (when (<= version 4)
         (setf (getf res :kdf-mode) (aref desc 3))
         (setf (getf res :padding-index) (aref desc 2))))
     res))
@@ -63,16 +70,34 @@
     desc))
 
 (defun authenticate-payload (passphrase version description iv ciphertext)
-  "Authenticate a crypto payload via HMAC.
+  "Authenticate a crypto payload via HMAC. Basically, HMAC everything in the
+   payload BESIDES the included HMAC.
    
    NOTE: this is now vestigial as all crypto version 5 and higher uses block
-   modes with built-in encryption (GCM)."
+   modes with built-in encryption (GCM).
+   
+   NOTE: if version <= 4, we mimic how the JS version of this function worked,
+   which is a buggy concatenation:
+     3 + 2 + 'asdf'
+   which was thought to yield '32asdf' however yielded '5asdf'. great. although
+   this function is only used for old data currently, it may be used again in
+   the future for authenticated crypto modes, so we implement the non-buggy
+   behavior as well as the buggy for backwards compat."
   (let* ((payload (concatenate 'nec:octet-array
-                               (babel:string-to-octets (write-to-string version))
-                               (length description)
+                               (if (<= version 4)
+                                   ;; implement buggy, JS-compatible version
+                                   ;; 3 + 2 + "desc" => "5desc"
+                                   (babel:string-to-octets (write-to-string (+ version (length description))))
+                                   ;; *correct* version of concatenation
+                                   ;; 3 + 2 + "desc" => #(0 3 2 "d" "e" "s" "c")
+                                   (vector (logand (ash version -8) #xff)
+                                           (logand version #xff)
+                                           (length description)))
                                description
                                iv
                                ciphertext)))
+    (format t "payload (~a): ~s~%" (length payload) payload)
+    (format t "hmac key: ~s~%" (to-base64 passphrase))
     (nec:hmac-sha256 passphrase payload)))
 
 (defun derive-keys (master-key &key (hasher :sha1) (iterations 50) (key-size 64))
@@ -123,9 +148,10 @@
 		;; we'll play it safe and cap at 1K
     (when (< 1000 version)
       (let* ((split (position (char-code #\:) enc))
-             (ciphertext (from-base64 (subseq enc 0 split)))
-             (iv (from-hex (subseq enc (1+ split))))
-             (res (list :ciphertext ciphertext
+             (ciphertext (from-base64 (babel:octets-to-string (subseq enc 0 split))))
+             (iv (from-hex (subseq enc (+ 2 split))))
+             (res (list :version 0
+                        :ciphertext ciphertext
                         :cipher :aes
                         :block-mode :cbc
                         :padding :ansix923
@@ -188,6 +214,10 @@
                          (cipher (aref +cipher-index+ +default-cipher+))
                          (block-mode (aref +block-index+ +default-block+)))
   "Encrypt the given plaintext, and serialize the result in our standard format."
+  ;; TODO: implement switchable ciphers/block modes
+  (setf cipher :aes)
+  (setf block-mode :gcm)
+
   (assert (find cipher +cipher-index+))
   (assert (find block-mode +block-index+))
   (let ((version (if (zerop version)
@@ -209,26 +239,84 @@
              (serialized (serialize ciphertext 0 :iv iv)))
         (return-from encrypt serialized)))
 
+    ;; TODO: actually implement cipher/block-mode here. right now we just
+    ;; encrypt using the latest method available (GCM)
     (let* ((desc (encode-payload-description version :cipher cipher :block-mode block-mode))
            ;; note we pass ciphertext as #() here since we are going to use this
            ;; serialized data for authentication used to generate the ciphertext
            (auth (serialize #() version :desc desc :iv iv))
-           (ciphertext (nec:encrypt-aes-gcm key
-                                            plaintext
-                                            iv
-                                            auth)))
+           (ciphertext (nec:encrypt-aes-gcm key plaintext iv auth)))
       (concatenate 'nec:octet-array
                    auth
                    ciphertext))))
 
-(identity
-(encrypt (key-from-string "oPCkqo99Egq1kvkYAGcL0LnovZORZ2SBpcT49Flf9yI=")
-         (babel:string-to-octets "IGNORE ME!!!")
-         :iv (from-base64 "Cek8BUuta935WkoOWJKQIA=="))
-)
+(defun fix-utf-key (utf8-key)
+  "Wow. Such fail. In older versions of Turtl, keys were UTF8 encoded strings.
+   This function converts the keys back."
+  (let ((key-str (babel:octets-to-string utf8-key :encoding :utf-8))
+        (bytes (make-array 32 :element-type 'nec:octet)))
+    (loop for i from 0
+          for c across key-str do
+      (setf (aref bytes i) (char-code c)))
+    bytes))
 
-(defun decrypt (key ciphertext &key (version +crypto-version+) iv)
-  )
+(defun decrypt (key ciphertext)
+  "Given a key and a ciphertext (both byte vectors), decrypt the ciphertext and
+   return the result (a byte vector).
+   
+   All the information needed to decrypt the ciphertext (cipher, block mode, iv,
+   padding, key derivation method, etc) is stored in the ciphertext itself."
+  ;; due to some errors in judgement, early keys were encoded as utf8 strings.
+  ;; luckily we can detect them and fix them when decrypting.
+  (when (< 32 (length key))
+    (setf key (fix-utf8-key key)))
+  (let* ((enc (deserialize ciphertext))
+         (version (getf enc :version))
+         (desc (if (zerop version)
+                   (list :cipher :aes
+                         :block-mode :cbc)
+                   (decode-payload-description version (getf enc :desc))))
+         (iv (getf enc :iv))
+         (payload (getf enc :ciphertext))
+         (decrypted nil))
+    (cond ((zerop version)
+           (setf decrypted (nec:decrypt-aes-cbc key payload iv)))
+          ((<= version 4)
+           (let* ((kdf-entry (aref +kdf-index+ (getf desc :kdf-mode)))
+                  (keys (derive-keys key
+                                     :hasher (aref kdf-entry 0)
+                                     :iterations (aref kdf-entry 1)
+                                     :key-size (aref kdf-entry 2)))
+                  (crypto-key (getf keys :crypto))
+                  (hmac-key (getf keys :hmac))
+                  (payload-hmac (getf enc :hmac))
+                  (compare-hmac (authenticate-payload hmac-key
+                                                      version
+                                                      (getf enc :desc)
+                                                      iv
+                                                      payload)))
+             (unless (nec:secure-equal payload-hmac compare-hmac)
+               (error 'crypto-authentication-failure :msg "Payload HMACs did not match (either you have tampered data or a bad key)"))
+             (setf decrypted (nec:decrypt-aes-cbc crypto-key payload iv))))
+          ((<= 5 version)
+           (let ((auth (deserialize ciphertext :raw t)))
+             (handler-case
+               (setf decrypted (nec:decrypt-aes-gcm key payload iv auth))
+               (nec:nettle-auth-error ()
+                 (error 'crypto-authentication-failure :msg "GCM authentication failed."))))))
+
+    ;; made it. if were version 4 or 5, remove the leading utf8 byte. this is an
+    ;; obnoxious vestigial requirement from the JS/tcrypt days.
+    (when (<= 4 version 5)
+      (setf decrypted (subseq decrypted 1)))
+    decrypted))
+
+(defun ctest()
+(let* ((key (key-from-string "wqBkfcKUwqbCpcKiHFsqLsOzL8O3wqAEfMKpc8OsCWMtQC4iecOHwobDg1fDtw=="))
+       (ciphertext (from-base64 "AAPaSAJDTCyvIFeveVQEiaSPhVmLauA8AugseiHwtya1uwQAAAAAnABM9+1DPTYxbpiNnA3oJIOfJi2TGkduA9lDKLjT9eq3k5cHnka+l1HXP9b295WS25AxO3rn5/jbnUUOKXCPmLvJnvcyc3ba42WnjF8NsyMws8QnIli78MxnrCvZWWG1tNgpbg4TlfnoBKRgp+d5MLpuCe1LqHYHdPU2NjaLwF8WVhK0HnHBuM6/CMzgBW/nOn1Cb5O4yfhUoGzUiVDa1AooZ+0iL8vBIkJXI7ATXnhLSUMXnyQUJcD5vcbKGMaC8Sb5/OjR80GFEMjTDREPxx1rZcWoUYfudv5mVEo1HZYlmqt4qeNb0sVVnz/bzzunn/o7fp2tvVkK1jR/MGeHNPWYeTPeTHgD7EH5hJAxygnaIzUDpCYraEYvycnpORwF694LuykW+CFTa4tlTReE+6Z0zlC2K639lAn3uDm0QuS85Jmd8XvZyh1w3GOs1S94IlgUnbDX4zrzxvC6AEFaUcuCA6CrP5rksMM4FdEvQSE7Cz6RPNQR7CEmxmUKH7NxPPAYPCkuVHfXSPk/dq7aq/j2vfcfHNvSVTmQ0I7QWQk6PFmA1rnR46MhlPBTaViO"))
+       (plain (decrypt key ciphertext)))
+  (format t "dec: ~s~%" (babel:octets-to-string plain)))
+)
 
 (defun keygen (passphrase salt iterations key-size &key (hasher :sha1))
   "Generate a key from a passphrase using PBKDF2."
